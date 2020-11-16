1d35c25a57bee7ebebd5a4b1296674110161a408
wu-jy17 <2017050026@secoder.net>
2020-11-16 14:36:12 +0800

    start

---
+++ cmake-build-debug/CMakeFiles/system_software.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/system_software.dir/main.cpp.o"
+  "system_software"
+  "system_software.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/system_software.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/system_software.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for system_software.
+# This may be replaced when dependencies are built.
